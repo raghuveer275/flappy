bfc70016ca69e22603a681452e26bddbf5963917
Raghuveer Bharadwaj <raghuveer.mh@cognitiveclouds.com>
2020-02-24 00:35:59 +0530

    Updates

--- precache-manifest.76a024c35210c4d123cf581cd5116445.js
+++ precache-manifest.76a024c35210c4d123cf581cd5116445.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5c7114db5ed7c9c1b3d7e65b4f9c86b",
+    "revision": "7dba4437a8bddb107c49a749e967a346",
     "url": "/flappy/index.html"
   },
   {
-    "revision": "1076dfc27218d9417434",
+    "revision": "a50905dd698d652e337a",
     "url": "/flappy/static/css/main.3cc9f78f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flappy/static/js/2.242de3a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1076dfc27218d9417434",
-    "url": "/flappy/static/js/main.62deb433.chunk.js"
+    "revision": "a50905dd698d652e337a",
+    "url": "/flappy/static/js/main.9aab7197.chunk.js"
   },
   {
     "revision": "c0479cb4ba1ab39592c1",
